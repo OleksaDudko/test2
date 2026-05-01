3e8315b2c5f66081910794f93ff54630690e6273
OleksaDudko <132586603+OleksaDudko@users.noreply.github.com>
2026-05-01 19:20:43 +0300

    test2

---
+++ app.js
@@ -0,0 +1,50 @@
+const inputEl = document.querySelector("#bookmarkInput");
+const btnEl = document.querySelector("#addBookmarkBtn");
+const listEl = document.querySelector("#bookmarkList");
+
+
+const STORAGE_KEY = "inputValue";
+
+
+const bookArrey = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+createBookItems(bookArrey);
+
+btnEl.addEventListener("click", () => {
+    const value = inputEl.value;
+    bookArrey.push(value);
+    savedData();
+    inputEl.value = "";
+    createBookItems(bookArrey);
+})
+
+
+function createBookItems(arr) {
+    const item = arr.map((elem, index) => {
+        return `<li id="${index}">
+                    <a href="#">${elem}</a>
+                    <div>
+                    <button type="button">Видалити</button>
+                    <button type="button">Редагувати</button>
+                    </div>
+                </li>`;
+    }).join("");
+    listEl.innerHTML = item;
+}
+
+
+listEl.addEventListener("click", (event) => {
+    if (event.target.nodeName !== "BUTTON") {
+        return
+    }
+    // const li = event.target.parentNode;
+    const li = event.target.closest("li")
+    const id = li.id;
+    bookArrey.splice(id, 1);
+    savedData();
+    createBookItems(bookArrey);
+})
+
+
+function savedData() {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(bookArrey));
+}
